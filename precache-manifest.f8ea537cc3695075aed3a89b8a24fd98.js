5b3ed6b06db9ca2061cccc814e08ab92e54441a7
sabiqthottoly <sabiqt@gmail.com>
2021-03-28 22:55:15 +0400

    Updates

---
+++ precache-manifest.f8ea537cc3695075aed3a89b8a24fd98.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b6aeed0ed569f1c7e0a7e81ad2d959",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "65254dbf78e0cc2c49cf",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "487cc13368c8ff8e52f0",
+    "url": "/react-frontend-dev-portfolio/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "65254dbf78e0cc2c49cf",
+    "url": "/react-frontend-dev-portfolio/static/js/2.7ebf5745.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.7ebf5745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487cc13368c8ff8e52f0",
+    "url": "/react-frontend-dev-portfolio/static/js/main.402d0495.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  },
+  {
+    "revision": "0777e063fc20b1143ca47b3dbcc39e3c",
+    "url": "/react-frontend-dev-portfolio/static/media/myProfile.0777e063.jpg"
+  },
+  {
+    "revision": "1668774e9b341da57a4f4ffe389870f5",
+    "url": "/react-frontend-dev-portfolio/static/media/p1.1668774e.png"
+  },
+  {
+    "revision": "4c10a90bb551c735b36c232d444bed16",
+    "url": "/react-frontend-dev-portfolio/static/media/p1.4c10a90b.png"
+  },
+  {
+    "revision": "9d1ff335a88c013f7d5efd41e9279955",
+    "url": "/react-frontend-dev-portfolio/static/media/p1.9d1ff335.png"
+  },
+  {
+    "revision": "eb188855f55300528902cb6b8fd11065",
+    "url": "/react-frontend-dev-portfolio/static/media/p2.eb188855.png"
+  }
+]);
